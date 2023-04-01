304c61092e5d5f6fc12a6e62a2322b971a759864
dommangetnicolas <contact@nicolas-dmg.fr>
2023-04-01 12:24:57 +0200

    feat: remove name

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -3,11 +3,6 @@ import { IsNotEmpty, IsString, MinLength } from 'class-validator';
 export class CreateUserDto {
   @IsString()
   @IsNotEmpty()
-  @MinLength(3)
-  name: string;
-
-  @IsString()
-  @IsNotEmpty()
   phone: string;
 
   @IsString()
